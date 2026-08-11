ae6a8d60f40e99b937aa34f6076434ab7ab4f162
Rain <rainkalugdan@gmail.com>
2026-08-11 14:56:01 +0800

    refactor: map over links

--- src/app/(public)/components/Navigation.tsx
+++ src/app/(public)/components/Navigation.tsx
@@ -12,33 +12,32 @@ export default function Navigation() {
       : "text-primary-foreground/90 font-bold text-sm border-b-2 border-transparent px-5 py-2 hover:text-primary-foreground transition-colors";
   }
 
+  const links = [
+    { href: "/", label: "Home", active: pathname === "/" },
+    { href: "about", label: "About", active: pathname === "/about" },
+    { href: "meetings", label: "Meetings", active: pathname === "/meetings" },
+    {
+      href: "membership",
+      label: "Membership",
+      active: pathname === "/membership",
+    },
+    {
+      href: "contact",
+      label: "Contact",
+      active: pathname === "/contact",
+    },
+  ];
 
   return (
-    <nav className="flex gap-4 items-center font-heading">
-      <Link href="/" className={navLinkClassName(pathname === "/")}>
-        Home
-      </Link>
-      <Link href="about" className={navLinkClassName(pathname === "/about")}>
-        About
-      </Link>
-      <Link
-        href="meetings"
-        className={navLinkClassName(pathname === "/meetings")}
-      >
-        Meetings
-      </Link>
-      <Link
-        href="membership"
-        className={navLinkClassName(pathname === "/membership")}
-      >
-        Membership
-      </Link>
-      <Link
-        href="contact"
-        className={navLinkClassName(pathname === "/contact")}
-      >
-        Contact
-      </Link>
-    </nav>
+    <div className="relative font-heading">
+      <nav className="flex gap-4 items-center">
+        {/* Desktop Navigation */}
+        {links.map((link) => (
+          <Link href={link.href} className={navLinkClassName(link.active)}>
+            {link.label}
+          </Link>
+        ))}
+      </nav>
+    </div>
   );
 }

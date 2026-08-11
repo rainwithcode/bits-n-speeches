6e113ce4ffd8badf26127222f07b0122779dec9f
Rain <rainkalugdan@gmail.com>
2026-08-11 16:15:08 +0800

    refactor: adjust header padding for smaller screen sizes

--- src/app/(public)/components/Header.tsx
+++ src/app/(public)/components/Header.tsx
@@ -6,10 +6,10 @@ import toastmastersLogo from "@/imports/toastmasters-logo_2x.png";
 export default function Header() {
   return (
     <header className="bg-primary">
-      <div className="flex justify-between items-center max-w-7xl mx-auto px-8 py-6">
+      <div className="flex justify-between items-center max-w-7xl mx-auto p-4 md:p-6 lg:p8">
         <Link
           href="/"
-          className="flex items-center gap-4 text-primary-foreground font-heading font-bold text-2xl tracking-wide"
+          className="flex items-center gap-4 text-primary-foreground font-heading font-bold text-lg md:text-2xl tracking-wide"
         >
           <Image
             src={toastmastersLogo}

6919574a87e0d247cac403c86982e13e7c1d0f11
Rain <rainkalugdan@gmail.com>
2026-08-11 14:54:50 +0800

    refactor: decrease vertical header padding

--- src/app/(public)/components/Header.tsx
+++ src/app/(public)/components/Header.tsx
@@ -1,17 +1,18 @@
 import Link from "next/link";
+import Image from "next/image";
 import Navigation from "./Navigation";
 import toastmastersLogo from "@/imports/toastmasters-logo_2x.png";
 
 export default function Header() {
   return (
     <header className="bg-primary">
-      <div className="flex justify-between items-center max-w-7xl mx-auto p-8">
+      <div className="flex justify-between items-center max-w-7xl mx-auto px-8 py-6">
         <Link
           href="/"
           className="flex items-center gap-4 text-primary-foreground font-heading font-bold text-2xl tracking-wide"
         >
-          <img
-            src={toastmastersLogo.src}
+          <Image
+            src={toastmastersLogo}
             alt="Toastmasters logo"
             className="w-auto h-12"
           />

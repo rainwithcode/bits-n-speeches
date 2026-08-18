d1e677cdf9c2590aefb7f2a1d3b201b7e1cc7feb
Rain <rainkalugdan@gmail.com>
2026-08-18 14:40:48 +0800

    feat(footer): add navigation links and meeting details

--- src/app/(public)/components/Footer.tsx
+++ src/app/(public)/components/Footer.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { Clock, Map, MapPin, Pin, Video } from "lucide-react";
+
 import { siteConfig } from "@/app/data/site-config";
+import { meetings } from "@/app/data/meetings";
 import toastmastersLogo from "@/imports/toastmasters-logo_2x.png";
 
 import { navLinks, membershipLinks } from "../data/nav-links";
@@ -10,7 +13,7 @@ export default function Footer() {
   return (
     <footer className="bg-primary text-primary-foreground">
       <div className="max-w-7xl mx-auto p-4 md:p-6 lg:p8 ">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-8">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-8">
           <div>
             <Image
               src={toastmastersLogo}
@@ -26,12 +29,12 @@ export default function Footer() {
           </div>
           <nav className="contents" aria-label="Footer">
             <div>
-              <h2 className="text-accent font-heading font-bold mb-3">
-                Quick Links
-              </h2>
-              <ul className="space-y-2">
+              <ul>
+                <h2 className="text-accent font-heading font-bold mb-3">
+                  Quick Links
+                </h2>
                 {navLinks.map((link) => (
-                  <li key={link.href}>
+                  <li key={link.href} className="mb-2">
                     <Link
                       href={link.href}
                       className="text-lg font-semi-bold text-primary-foreground/70 hover:text-primary-foreground transition-colors"
@@ -40,6 +43,62 @@ export default function Footer() {
                     </Link>
                   </li>
                 ))}
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <h2 className="text-accent font-heading font-bold mb-3">
+                  Join Us
+                </h2>
+                {membershipLinks.map((link) => (
+                  <li className="mb-2" key={link.href}>
+                    <Link
+                      href={link.href}
+                      className="text-lg font-semi-bold text-primary-foreground/70 hover:text-primary-foreground transition-colors"
+                    >
+                      {link.label}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <ul className="font-semi-bold text-primary-foreground/70">
+                <h2 className="text-accent font-heading font-bold mb-3">
+                  Visit Us
+                </h2>
+                <li className="text-lg mb-2" key="schedule">
+                  <div className="flex gap-2">
+                    <Clock className="w-5 text-accent" />
+                    <span className="text-primary-foreground">
+                      Schedule:
+                    </span>{" "}
+                  </div>
+                  Every {meetings.day} at {meetings.time}
+                </li>
+                <li className="text-lg mb-2" key="in-person">
+                  <div className="flex gap-2">
+                    <MapPin className="w-5 text-accent" />
+                    <span className="text-primary-foreground">
+                      In Person (1st &amp; 3rd {meetings.day}s):
+                    </span>{" "}
+                  </div>
+                  {meetings.location.inPerson}
+                </li>
+                <li className="text-lg mb-2" key="online">
+                  <div className="flex gap-2">
+                    <Video className="w-5 text-accent" />
+                    <span className="text-primary-foreground">
+                      {meetings.location.online}:
+                    </span>{" "}
+                  </div>
+                  <a
+                    href="mailto:bitsspeeches@gmail.com?subject=Meeting%20Link%20Request&body=Hi%2C%20could%20you%20send%20me%20the%20link%20to%20join%20the%20next%20meeting%3F"
+                    className="text-accent underline hover:text-primary-foreground transition-colors"
+                  >
+                    Email us for link
+                  </a>
+                </li>
               </ul>
             </div>
           </nav>

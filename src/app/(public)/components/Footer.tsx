1ffe09b2cc047b8ff76346504c3bbab7f86bc2a0
Rain <rainkalugdan@gmail.com>
2026-08-18 14:43:31 +0800

    feat(footer): increase vertical padding

--- src/app/(public)/components/Footer.tsx
+++ src/app/(public)/components/Footer.tsx
@@ -12,7 +12,7 @@ import { navLinks, membershipLinks } from "../data/nav-links";
 export default function Footer() {
   return (
     <footer className="bg-primary text-primary-foreground">
-      <div className="max-w-7xl mx-auto p-4 md:p-6 lg:p8 ">
+      <div className="max-w-7xl mx-auto px-4 md:px-6 lg:px-8 py-12 ">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-8">
           <div>
             <Image

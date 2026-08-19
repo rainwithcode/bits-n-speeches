bea11c74d2122932fbdf0e5991ad201d8de21ab9
Rain <rainkalugdan@gmail.com>
2026-08-19 16:32:17 +0800

    feat(footer): change background to dark

--- src/app/(public)/components/Header.tsx
+++ src/app/(public)/components/Header.tsx
@@ -8,7 +8,7 @@ import Navigation from "./Navigation";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 bg-primary">
+    <header className="sticky top-0 z-50 bg-surface-dark">
       <div className="flex flex-wrap justify-between items-center max-w-7xl mx-auto p-4 md:p-6 lg:p-8">
         <Link
           href="/"

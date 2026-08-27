a8b60f4c74ac31910beaed7683a4c7587140167c
Rain <rainkalugdan@gmail.com>
2026-08-27 12:09:52 +0800

    feat(view-details-button): add variants

--- src/components/ui/ViewDetailsButton.tsx
+++ src/components/ui/ViewDetailsButton.tsx
@@ -1,7 +1,23 @@
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
 
+const viewDetailsButtonVariants = cva(
+  "flex items-center gap-2 w-fit px-4 py-2 rounded-md",
+  {
+    variants: {
+      variant: {
+        primary: "bg-accent text-primary",
+        secondary: "bg-accent text-primary",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+    },
+  },
+);
 export default function ViewDetailsButton({
   href,
   children,

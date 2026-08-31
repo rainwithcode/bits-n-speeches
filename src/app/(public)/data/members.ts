c5f2a349a8db19ff4df71b3c554447023da37e07
Rain <rainkalugdan@gmail.com>
2026-08-31 11:39:33 +0800

    feat: update member information

--- src/app/(public)/data/members.ts
+++ src/app/(public)/data/members.ts
@@ -11,7 +11,7 @@ export const members: Member[] = [
     image: "brian-hatano.jpg",
     profession: "Toastmasters Mentor",
     testimonial:
-      "Since I joined Toastmasters in 2001, I've enjoyed the fellowship and learning life-changing skills that help me both professionally and personally. Serving has a mentor to several people has been one of the most rewarding experiences.",
+      "Since I joined Toastmasters in 2001, I've enjoyed the fellowship and learning life-changing skills that help me both professionally and personally. Serving as a mentor to several people has been one of the most rewarding experiences.",
   },
   {
     name: "Dorothy Uzoh",
@@ -29,41 +29,55 @@ export const members: Member[] = [
   },
   {
     name: "Rosemary Okeke",
+    image: "rosemary-okeke.jpg",
     testimonial:
       "Joining Bits ’N Speeches has been an amazing experience! I am gaining confidence speaking in front of others, met amazing people, and growing both personally and professionally. I love being part of such a supportive and inspiring community.",
   },
   {
     name: "Edward Johnson",
+    image: "edward-johnson.jpg",
     testimonial:
       'Aside from being a club that excels in training, procedure, participation and distinction, this is the most comfortable club in Toastmasters with no pressures or intimidation. It\'s like a "framily" (friends and family).',
   },
   {
     name: "Jaime Vargas",
+    image: "jaime-vargas.jpg",
     testimonial:
       "I joined to become a better speaker and leader. Over time, it's helped me build confidence, structure my thoughts better, and step into leadership roles.",
   },
   {
     name: "Susan Sontra",
+    image: "susan-sontra.jpg",
     testimonial:
       "Being a member of BNS (Bits ’N Speeches) has brought growth and skills I didn't expect. I've gained confidence in speaking and learned leadership and project management in fun, challenging ways.",
   },
   {
     name: "Kristen Bahler",
+    image: "kristen-bahler.jpg",
     testimonial:
       "Joining Bits ’N Speeches has given me the confidence to speak up more in work settings, and becoming willing to teach others about the public speaking skills that will benefit them in their future endeavors.",
   },
   {
     name: "Rain Kalugdan",
+    image: "rain-kalugdan.jpg",
     testimonial:
       "Bits ’N Speeches has helped me overcome my fear of public speaking and discover how I can use my voice to help others.",
   },
   {
     name: "Douglas Bloom",
+    image: "douglas-bloom.jpg",
+  },
+  {
+    name: "Samantha Lam",
+    image: "samantha-lam.jpg",
+  },
+  {
+    name: "Rebecca Kendrick",
+    image: "rebecca-kendrick.jpg",
   },
   {
     name: "Cheryl Isaac",
   },
-
   {
     name: "Mark Graham",
   },

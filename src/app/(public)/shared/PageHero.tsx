fcd522f06726dc9e132cb14bd7406149388a3cc7
Rain <rainkalugdan@gmail.com>
2026-09-01 11:36:11 +0800

    refactor(page-hero): wrap around section

--- src/app/(public)/shared/PageHero.tsx
+++ src/app/(public)/shared/PageHero.tsx
@@ -6,7 +6,7 @@ export default function PageHero({
   description: string;
 }) {
   return (
-    <div className="bg-primary">
+    <section className="bg-primary">
       <div className="max-w-7xl mx-auto px-4 md:px-6 lg:px-8 py-8 md:py-16">
         <h1 className="text-primary-foreground text-2xl md:text-4xl font-heading font-bold">
           {title}
@@ -15,6 +15,6 @@ export default function PageHero({
           {description}
         </p>
       </div>
-    </div>
+    </section>
   );
 }

cf08fad294ccd12354992304d5ba7a8f005e75da
Rain <rainkalugdan@gmail.com>
2026-09-01 12:42:31 +0800

    refactor(badge): add variants

--- src/components/ui/Badge.tsx
+++ src/components/ui/Badge.tsx
@@ -1,8 +1,15 @@
 import { ReactNode } from "react";
 
-export default function Badge({ children }: { children: ReactNode }) {
+type BadgeProps = {
+  variant?: "default" | "highlight";
+  children: ReactNode;
+};
+
+export default function Badge({ variant = "default", children }: BadgeProps) {
   return (
-    <div className="w-fit bg-accent text-sm text-primary font-bold px-2 mb-4 rounded-md">
+    <div
+      className={`w-fit text-sm font-bold px-2 mb-4 rounded-md ${variant === "default" ? "bg-primary text-primary-foreground" : "bg-accent text-primary"}`}
+    >
       {children}
     </div>
   );

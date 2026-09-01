547faa71b5440f2e31965c7382a78643b366a39d
Rain <rainkalugdan@gmail.com>
2026-09-01 11:38:36 +0800

    refactor: remove console log

--- src/app/(public)/about/components/Leadership.tsx
+++ src/app/(public)/about/components/Leadership.tsx
@@ -4,7 +4,6 @@ import SectionHeading from "../../shared/SectionHeading";
 import PersonCard from "./PersonCard";
 
 export default function Leadership() {
-  console.log(memberWithRoles);
   return (
     <section className="bg-white">
       <div className="max-w-7xl mx-auto px-4 md:px-6 lg:px-8 py-8 md:py-16">

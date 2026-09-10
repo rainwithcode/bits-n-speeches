103b3ed67dccd7e1869db685dade746050a602d7
Rain <rainkalugdan@gmail.com>
2026-09-10 14:30:13 +0800

    refactor(icon-card): increase mobile icon

--- src/components/ui/IconCard.tsx
+++ src/components/ui/IconCard.tsx
@@ -14,7 +14,7 @@ export default function IconCard({
   return (
     <div className="space-y-2 md:space-y-4 bg-white p-8 rounded-md border border-border">
       <div className="w-fit px-4 py-4 rounded-md bg-primary/10">
-        <Icon className="w-4 h-4 md:w-8 md:h-8 text-primary" />
+        <Icon className="w-6 h-6 md:w-8 md:h-8 text-primary" />
       </div>
       <h3 className="font-heading font-bold text-base md:text-xl text-primary">
         {label}

89134b8ad6145a5a9bb593ab1082ce3f4b0498b0
Rain <rainkalugdan@gmail.com>
2026-09-10 14:27:54 +0800

    refactor(icon-card): align icon left

--- src/components/ui/IconCard.tsx
+++ src/components/ui/IconCard.tsx
@@ -13,8 +13,8 @@ export default function IconCard({
 }: IconCardProps) {
   return (
     <div className="space-y-2 md:space-y-4 bg-white p-8 rounded-md border border-border">
-      <div className="mx-auto w-fit px-4 py-4 rounded-md bg-primary/10">
-        <Icon className="w-8 h-8 text-primary" />
+      <div className="w-fit px-4 py-4 rounded-md bg-primary/10">
+        <Icon className="w-4 h-4 md:w-8 md:h-8 text-primary" />
       </div>
       <h3 className="font-heading font-bold text-base md:text-xl text-primary">
         {label}

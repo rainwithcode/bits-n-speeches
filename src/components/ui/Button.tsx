d4e0b5a080eeb69c373f135e5e5dc36901cb2fe7
Rain <rainkalugdan@gmail.com>
2026-09-19 15:13:10 +0800

    feat(button): disabled prop and styles

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,25 +4,29 @@ import { ReactNode } from "react";
 
 import { cn } from "@/lib/utils";
 
-const buttonVariants = cva("flex items-center gap-2 w-fit font-bold", {
-  variants: {
-    color: {
-      primary: "bg-primary text-primary-foreground hover:bg-secondary",
-      secondary:
-        "bg-white text-primary border border-primary hover:bg-secondary hover:text-primary-foreground",
-      accent:
-        "bg-accent text-primary hover:bg-secondary hover:text-primary-foreground",
+const buttonVariants = cva(
+  "flex items-center gap-2 w-fit font-bold cursor-pointer disabled:cursor-not-allowed",
+  {
+    variants: {
+      color: {
+        primary:
+          "bg-primary text-primary-foreground hover:bg-secondary disabled:hover:bg-primary",
+        secondary:
+          "bg-white text-primary border border-primary hover:bg-secondary hover:text-primary-foreground disabled:hover:bg-white disabled:hover:text-primary",
+        accent:
+          "bg-accent text-primary hover:bg-secondary hover:text-primary-foreground disabled:hover:bg-accent",
+      },
+      size: {
+        big: "px-6 py-4 md:px-8 md:py-4 text-sm md:text-lg",
+        small: "px-4 py-2",
+      },
     },
-    size: {
-      big: "px-6 py-4 md:px-8 md:py-4 text-sm md:text-lg",
-      small: "px-4 py-2",
+    defaultVariants: {
+      color: "primary",
+      size: "small",
     },
   },
-  defaultVariants: {
-    color: "primary",
-    size: "small",
-  },
-});
+);
 
 type BaseProps = VariantProps<typeof buttonVariants> & {
   className?: string;
@@ -38,6 +42,7 @@ type NativeButtonProps = BaseProps & {
   as: "button";
   type?: "button" | "submit" | "reset";
   onClick?: () => void;
+  disabled?: boolean;
 };
 
 type ButtonProps = LinkButtonProps | NativeButtonProps;
@@ -56,10 +61,8 @@ export default function Button({
       <button
         type={props.type}
         onClick={props.onClick}
-        className={cn(
-          "cursor-pointer disabled:cursor-not-allowed disabled:opacity-50",
-          classNameValue,
-        )}
+        className={cn(classNameValue)}
+        disabled={props.disabled}
       >
         {children}
       </button>

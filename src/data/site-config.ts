f3348a4cd14f82ec81aa407b9d0083e5850cf179
Rain <rainkalugdan@gmail.com>
2026-09-23 15:24:30 +0800

    refactor(site-config): add email and social to contact object and rename site key to organization url

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -6,22 +6,27 @@ export const siteConfig = {
   organization: "Toastmasters International",
   tagline:
     "A Toastmasters club dedicated to developing confident communicators and leaders.",
-
+  organizationUrl: "https://www.toastmasters.org/",
   // contact
-  email: "bitsspeeches@gmail.com",
-  site: "https://www.toastmasters.org/",
-  social: [
-    {
-      name: "Facebook",
-      username: "BitsnSpeeches",
-      href: "https://www.facebook.com/BitsnSpeeches",
-      icon: FaFacebook,
+  contact: {
+    email: {
+      address: "bitsspeeches@gmail.com",
+      subject: "Question about Bits ’N Speeches",
+      body: "Hi! I'd like to learn more about Bits ’N Speeches.",
     },
-    {
-      name: "Instagram",
-      username: "bits_nspeeches",
-      href: "https://www.instagram.com/bits_nspeeches/",
-      icon: FaInstagram,
-    },
-  ],
+    social: [
+      {
+        name: "Facebook",
+        username: "BitsnSpeeches",
+        href: "https://www.facebook.com/BitsnSpeeches",
+        icon: FaFacebook,
+      },
+      {
+        name: "Instagram",
+        username: "bits_nspeeches",
+        href: "https://www.instagram.com/bits_nspeeches/",
+        icon: FaInstagram,
+      },
+    ],
+  },
 };
